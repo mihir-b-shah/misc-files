415a9c8b63536d3c1d52e7e7ddd28eb3cfff1841
mihir-b-shah <46288414+mihir-b-shah@users.noreply.github.com>
2019-12-07 16:26:00 -0600

    balancing works

--- misc_contests/misc_cpp/balancing.cpp
+++ misc_contests/misc_cpp/balancing.cpp
@@ -16,63 +16,66 @@ int main() {
     fin >> n;
     const int N = n;
 
-    pair<int,int> xys[N];
+    pair<int,int> xs[N];
+    pair<int,int> ys[N];
 
     int x,y;
     for(int i = 0; i<N; ++i) {
         fin >> x >> y;
-        xys[i] = pair<int,int>{x,y};
+        xs[i] = pair<int,int>{x,y};
+        ys[i] = pair<int,int>{x,y};
     }
 
     fin.close();
 
-    sort(xys,xys+N);
+    sort(xs,xs+N);
+    sort(ys,ys+N,compare);
 
-    int opt_x = 0;
-    int diff = __INT_MAX__; 
-
-    for(int i = 0; i<N-1; ++i) {
-        if(xys[i].first == xys[i+1].first) continue;
-        else if(abs(N-(i<<1)-2)<diff) {
-            opt_x = xys[i].first+1;
-            diff = abs(N-(i<<1)-2);
-        }
-    }
-
-    sort(xys,xys+N,compare);
-    int opt_y = 0;
-    diff = __INT_MAX__;
+    int opt_y;
+    int output_opt = 1000000000;
 
     for(int i = 0; i<N-1; ++i) {
-        if(xys[i].second == xys[i+1].second) continue;
-        else if(abs(N-(i<<1)-2)<diff) {
-            opt_y = xys[i].second+1;
-            diff = abs(N-(i<<1)-2);
+        if(ys[i].second == ys[i+1].second) continue;
+        opt_y = ys[i].second+1;
+
+        int opt_x = 0;
+        int diff = __INT_MAX__; 
+        int ctrs[4] = {0};
+
+        for(int i = 0; i<N; ++i) {
+            if(xs[i].first < opt_x && xs[i].second < opt_y) {
+                ++ctrs[0]; // -x,-y
+            } else if(xs[i].first > opt_x && xs[i].second < opt_y) {
+                ++ctrs[1]; // +x,-y
+            } else if(xs[i].first < opt_x && xs[i].second > opt_y) {
+                ++ctrs[2]; // -x, +y
+            } else if(xs[i].first > opt_x && xs[i].second > opt_y) {
+                ++ctrs[3]; // +x, +y
+            }
         }
-    }
 
-    int ctrs[4] = {0};
+        for(int i = 0; i<N-1; ++i) {
+            if(xs[i].first == xs[i+1].first) continue;
+            opt_x = xs[i].first+1;
+            if(xs[i].second > opt_y) {
+                ++ctrs[2];
+                --ctrs[3];
+            } else {
+                ++ctrs[0];
+                --ctrs[1];
+            }
 
-    for(int i = 0; i<N; ++i) {
-        if(xys[i].first < opt_x && xys[i].second < opt_y) {
-            ++ctrs[0];
-        } else if(xys[i].first > opt_x && xys[i].second < opt_y) {
-            ++ctrs[1];
-        } else if(xys[i].first < opt_x && xys[i].second > opt_y) {
-            ++ctrs[2];
-        } else if(xys[i].first > opt_x && xys[i].second > opt_y) {
-            ++ctrs[3];
+            int output = 0;
+            for(int i = 0; i<4; ++i) {
+                output = max(output, ctrs[i]);
+            }
+
+            output_opt = min(output_opt, output);
         }
-    }
-
-    cout << "opt x: " << opt_x << " opt y: " << opt_y << endl;
-    int output = 0;
-    for(int i = 0; i<4; ++i) {
-        output = max(output, ctrs[i]);
     }
 
     ofstream fout("balancing.out");
-    fout << output << endl;
+    fout << output_opt << endl;
     fout.close();
     return 0;
 }
