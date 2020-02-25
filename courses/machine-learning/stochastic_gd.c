2b3c6364a49b7cc2a98aa01f7793f2e400f02d4e
mihir-b-shah <46288414+mihir-b-shah@users.noreply.github.com>
2020-02-25 14:50:44 -0600

    stoch grad desc works

--- courses/machine-learning/stochastic_gd.c
+++ courses/machine-learning/stochastic_gd.c
@@ -6,12 +6,11 @@
 #include <time.h>
 #include <string.h>
 #include <stdio.h> 
-#include <math.h>
 
 #define LAMBDA 1
 #define BETA 5
 #define NORM_BETA 0.2
-#define EPS 1e-3
+#define EPS 0.01
 
 bool init_rand = false;
 
@@ -27,10 +26,15 @@ void sgd(const int DIM, const int N, struct POINT* data, struct HYPERPLANE* fill
     if(!init_rand) {
         srand(time(NULL));
     }
-    float theta[DIM+1];
+    float* theta = fill->vector;
+    float* offset = &(fill->offset);
     float gradient[DIM+1];
-    memset(theta, 0, (DIM+1)*sizeof(float));
+
+    memset(theta, 0, DIM*sizeof(float));
+    *offset = 0;
     int iter = 0;
+    float loss_val;
+    float learn_rate;
 
     do {
         int index;
@@ -40,18 +44,22 @@ void sgd(const int DIM, const int N, struct POINT* data, struct HYPERPLANE* fill
         for(int i = 0; i<BETA; ++i) {
             index = rand_index(N);
             point = data + index;
+            loss_val = 0;
             for(int j = 0; j<DIM; ++j) {
-                printf("label: %d, coord: <%f, %f>\n", point->label, point->coord[0], point->coord[1]);
-                gradient[j] += NORM_BETA*(LAMBDA*theta[j]+fmaxf(0, -(point->label)*(point->coord[j])));
-                theta[j] -= learning_rate(iter)*gradient[j];
+                loss_val += point->coord[j]*theta[j];
+            }
+            loss_val += *offset;
+            loss_val *= point->label;
+            for(int j = 0; j<DIM; ++j) {
+                gradient[j] += NORM_BETA*(LAMBDA*theta[j]+(loss_val > 0 ? 0: -(point->label)*(point->coord[j])));
             }
             gradient[DIM] -= point->label;
-            theta[DIM] += learning_rate(iter)*gradient[DIM];
-            printf("Gradient: <%f, %f, %f>, Theta: <%f %f>, Offset: %f\n", gradient[0], gradient[1], gradient[2], 
-                                                                       theta[0], theta[1], theta[2]);
         }
+        learn_rate = learning_rate(iter);
+        for(int i = 0; i<DIM; ++i) {
+            theta[i] -= learn_rate*gradient[i];
+        }
+        *offset = learn_rate*gradient[DIM];
         ++iter;
-    } while(!dot(DIM, gradient, gradient) < EPS);
-    fill->vector = theta;
-    fill->offset = theta[DIM];
+    } while(learn_rate > EPS);
 }
